a5b7ad65b3549072c99ff717fb99cda231a3bb2c
vtserej <vtserej@gmail.com>
2022-11-03 20:17:12 -0400

    code

--- TestLearnProject/Form1.cs
+++ TestLearnProject/Form1.cs
@@ -6,5 +6,10 @@ namespace TestLearnProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
